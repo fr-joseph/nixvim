6a425fcca955091d67e4851be16e4a63b07cd934
Father Joseph <frjsph@proton.me>
2024-09-23 22:47:26 -0600

    Update lualine

--- config/lualine.nix
+++ config/lualine.nix
@@ -12,9 +12,10 @@
         # ignoreFocus = ["neo-tree"];
       };
       sections = {
-        lualine_a = [ "vim.fn.getcwd()" ];
+        # lualine_a = [ "vim.fn.getcwd()" ];
         # lualine_a = [ "string.gsub(vim.fn.getcwd(), '^/home/fj', '~')" ];
-        lualine_b = [ "filename" "filetype" ];
+        lualine_a.__raw = "{ { 'filename', file_status = true, path = 2 } }";
+        lualine_b = [ "filetype" ];
         lualine_c = [ "diff" "diagnostics" ];
         lualine_x = [ "encoding" "fileformat" ];
         lualine_y = [ "progress" "location" ];
